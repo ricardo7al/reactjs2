fa550aecde618417e693e133ffb6c280f642483f
unknown <ricardo7almail.com>
2020-04-02 13:54:12 -0300

    Updates

---
+++ precache-manifest.43db02ccbe6119e5089a03930a8a857a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae539bfdd18c81269e45fe81627028e6",
+    "url": "/ricardo7al/reactjs2/index.html"
+  },
+  {
+    "revision": "e0ecfbbbeafd6e7104ec",
+    "url": "/ricardo7al/reactjs2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2429c1bdc81e33213b8b",
+    "url": "/ricardo7al/reactjs2/static/js/2.dc2125b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ricardo7al/reactjs2/static/js/2.dc2125b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ecfbbbeafd6e7104ec",
+    "url": "/ricardo7al/reactjs2/static/js/main.b591fc68.chunk.js"
+  },
+  {
+    "revision": "47a89746d9a3bb71b807",
+    "url": "/ricardo7al/reactjs2/static/js/runtime-main.63fbe583.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ricardo7al/reactjs2/static/media/logo.5d5d9eef.svg"
+  }
+]);
